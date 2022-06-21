565fbff09808cbabb03b36d3e09ec6eeff25ec8e
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-21 08:36:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice21June1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a number between 1 to 10: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is: {0}",number);
+
+        }
+    }
+}
